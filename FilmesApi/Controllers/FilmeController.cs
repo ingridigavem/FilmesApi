7ef1693b657ae5b58dd5b3bbf1a088ebc4ef7c28
Ingridi Gave <ingridigavem@gmail.com>
2021-07-18 20:42:47 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,67 @@
+﻿using FilmesApi.Data;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase {
+
+        private FilmeContext _context;
+
+        public FilmeController(FilmeContext context) {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme) {
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaTodosOsFilmes() {
+            return Ok(_context.Filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id) {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme != null) {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaFilme(int id, [FromBody] Filme filmeAtualizado) {
+            var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null) {
+                return NotFound();
+            }
+            filme.Titulo = filmeAtualizado.Titulo;
+            filme.Diretor = filmeAtualizado.Diretor;
+            filme.Genero = filmeAtualizado.Genero;
+            filme.Duracao = filmeAtualizado.Duracao;
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaFilme(int id) {
+            var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null) {
+                return NotFound();
+            }
+            _context.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
+
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Models {
+    public class Filme {
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Título é obrigatório.")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo Diretor é obrigatório.")]
+        public string Diretor { get; set; }
+
+        [StringLength(50, ErrorMessage = "O gênero não pode passar de 50 caracteres.")]
+        public string Genero { get; set; }
+
+        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 minuto e no máximo 600 minutos.")]
+        public int Duracao { get; set; }
+    }
+}
